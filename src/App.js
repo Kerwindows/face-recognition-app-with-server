cb917105c95333a7b45efd28a0c60baeb2bd176b
kerwindows <kerwindows@hotmail.com>
2022-11-13 13:10:55 -0500

    inital commit. added serv er folder

---
+++ src/App.js
@@ -0,0 +1,147 @@
+import React, { useState } from "react";
+import ParticlesBg from "particles-bg";
+import Clarifai from "clarifai";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import Navigation from "./components/Navigation/Navigation";
+import Signin from "./components/Signin/Signin";
+import Register from "./components/Register/Register";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import "./App.css";
+
+//You must add your own API key here from Clarifai.
+const app = new Clarifai.App({
+  apiKey: "82b1d3173bd34131ba5e6fc16a0b8604",
+});
+
+const App = () => {
+  const [input, setInput] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [box, setBox] = useState("");
+  const [route, setRoute] = useState("signin");
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [user, setUser] = useState({
+    id: "",
+    name: "",
+    email: "",
+    entries: 0,
+    joined: "",
+  });
+
+  const loadUser = (data) => {
+    setUser({
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined,
+    });
+  };
+  //added to check if server is being read
+  // const componemtDidMount = () => {
+  //   fetch("http://localhost:3000/")
+  //     .then((response) => response.json())
+  //     .then(console.log);
+  // };
+  // componemtDidMount();
+
+  const calculateFaceLocation = (data) => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
+
+  const displayFaceBox = (box) => {
+    setBox(box);
+  };
+
+  const onInputChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const onButtonSubmit = () => {
+    setImageUrl(input);
+    app.models
+      .predict(
+        // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
+        // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
+        // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
+        // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
+        // is to use a different version of their model that works like the ones found here: https://github.com/Clarifai/clarifai-javascript/blob/master/src/index.js
+        // so you would change from:
+        // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+        // to:
+        // .predict('53e1df302c079b3db8a0a36033ed2d15', this.state.input)
+        Clarifai.FACE_DETECT_MODEL,
+        input
+      )
+      .then((response) => {
+        console.log(response);
+        if (response) {
+          if ("regions" in response.outputs[0].data) {
+            fetch("http://localhost:3000/image", {
+              method: "put",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify({
+                id: user.id,
+              }),
+            })
+              .then((response) => response.json())
+              .then((count) => {
+                //setUser(Object.assign(user, { entries: count })); //somehow this method does not update state until the input is cleared
+                setUser({ ...user, entries: count }); //this method works better
+              });
+            displayFaceBox(calculateFaceLocation(response));
+          } else {
+            console.log("cannot find face");
+          }
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const onRouteChange = (route) => {
+    if (route === "signout") {
+      setIsSignedIn(false);
+      setRoute("signin");
+    } else if (route === "home") {
+      setIsSignedIn(true);
+      setRoute("home");
+    } else {
+      setRoute(route);
+    }
+  };
+
+  return (
+    <div className="App">
+      <ParticlesBg type="cobweb" bg={true} />
+      <Navigation isSignedIn={isSignedIn} onRouteChange={onRouteChange} />
+      {route === "home" ? (
+        <div>
+          <Logo />
+          <Rank name={user.name} entries={user.entries} />
+          <ImageLinkForm
+            onInputChange={onInputChange}
+            onButtonSubmit={onButtonSubmit}
+          />
+          <FaceRecognition box={box} imageUrl={imageUrl} />
+        </div>
+      ) : route === "signin" ? (
+        <Signin loadUser={loadUser} onRouteChange={onRouteChange} />
+      ) : (
+        <Register loadUser={loadUser} onRouteChange={onRouteChange} />
+      )}
+    </div>
+  );
+};
+
+export default App;
